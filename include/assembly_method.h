e6ea157e1583e738558528e4497aac7ee6e25f64
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-06-23 00:05:01 +0100

    Add files via upload

---
+++ include/assembly_method.h
@@ -0,0 +1,12 @@
+#ifndef ASM_H
+#define ASM_H
+
+#define VARREG_BYTE(VAR, REG)   asm("move.b, %0, %/"REG"" :: "a" (VAR) : ""REG"")
+#define VARREG_WORD(VAR, REG)   asm("move.w, %0, %/"REG"" :: "a" (VAR) : ""REG"")
+#define VARREG_LONG(VAR, REG)   asm("move.l, %0, %/"REG"" :: "a" (VAR) : ""REG""))
+
+#define REGVAR_BYTE(REG, VAR)   asm("move.b, %0, %/"REG", %0" : "=a" (VAR))
+#define REGVAR_WORD(REG, VAR)   asm("move.w, %0, %/"REG", %0" : "=a" (VAR)) 
+#define REGVAR_LONG(REG, VAR)   asm("move.l, %0, %/"REG", %0" : "=a" (VAR))
+
+#endif 
