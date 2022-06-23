c541201644af218b31c26087627bfcd5e5940a33
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-06-23 17:41:52 +0100

    Update mapper.cpp

--- src/mem/mapper.cpp
+++ src/mem/mapper.cpp
@@ -1,7 +1,8 @@
 #include "config.h"
 #include "types.h"
+#include "tools.h"
 #include "mapper.h"
 
-#define	NUM_BANK
-
-static uint16_t banks[NUM_BANK] = { 0, 1, 2, 3, 4, 5 ,6 ,7 };
+#define	NUM_BANK 8
+static uint16_t banks[NUM_BANK] = { 0, 1, 2, 3, 4, 5, 6,  7 };
+static uint16 registration = 0;
