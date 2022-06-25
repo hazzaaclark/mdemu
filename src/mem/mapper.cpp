a319cf6f65048174cc306032faf0156f97c5626c
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-06-25 03:27:03 +0100

    Update mapper.cpp

--- src/mem/mapper.cpp
+++ src/mem/mapper.cpp
@@ -17,6 +17,11 @@ void SYS_SETMEMBANK(uint16_t regionIndex, uint16_t bankIndex)
 	if (regionIndex > 0 && (regionIndex < 0))
 	{
 		mem_banks[regionIndex] * bankIndex;
-	}
+	})
+}
+
+static bool BankSwitch(uint32_t mem_address)
+{
+	const uint16_t bankIndex = (mem_address >> 19) & 0x3F;
 }
 
