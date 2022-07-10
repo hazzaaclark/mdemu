320858cbda7bf60eec6f2162ced16a415278d740
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-07-10 19:31:17 +0100

    Update mapper.cpp

--- src/mem/mapper.cpp
+++ src/mem/mapper.cpp
@@ -1,28 +1,28 @@
-#include "config.h"
-#include "types.h"
-#include "tools.h"
-#include "mapper.h"
+#include "config.hpp"
+#include "mapper.hpp"
+#include "types.hpp"
+#include "tools.hpp"
+#include <cstdint>
 
 #define	NUM_BANK 8
-uint16_t mem_banks[NUM_BANK] = { 0, 1, 2, 3, 4, 5 ,6 ,7 };
-uint16_t mem_registration = 0;
+uint16_t data_banks[NUM_BANK] = { 0, 1, 2, 3, 4, 5 ,6 ,7 };
+uint16_t data_registration = 0;
 
-uint16_t SYS_GETMEMBANK(uint16_t regionIndex)
+static uint16_t SYS_GETDATABANK(uint16_t regionIndex)
 {
-	return mem_banks(regionIndex);
+	return data_banks[regionIndex];
 }
 
-void SYS_SETMEMBANK(uint16_t regionIndex, uint16_t bankIndex)
+void SYS_SETDATABANK(uint16_t regionIndex, uint16_t bankIndex)
 {
 	if (regionIndex > 0 && (regionIndex < 0))
 	{
-		mem_banks[regionIndex] * bankIndex;
+		data_banks[regionIndex] * regionIndex;
 	}
 }
 
 static bool BankSwitch(uint32_t mem_address)
 {
-	const uint16_t mem_mask(mem_address >> 16) & 0xFFF0;
-	return mem_mask(>= = 0x030 && (mem_mask < 0x0E0);
+	const uint16_t mem_mask(mem_address >> 16 & 0xFFF0);
+	return mem_mask >=  0x030 && (mem_mask < 0x0E0);
 }
-
