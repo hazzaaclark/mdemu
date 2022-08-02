bed5e70561d41dfa05fb4d054f8ec8070c474d19
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-08-02 23:09:04 +0100

    Update 68000.cpp

--- src/cpu/68000.cpp
+++ src/cpu/68000.cpp
@@ -1,31 +1,8 @@
-#include "68000.hpp"
-#include "bus.hpp"
+#include "68000.h"
 #include <cstdint>
+#include <stdio.h>
 
-/// Initiator
-
-68000::68000()
+static void SET_CPU_STATE(STATE *state)
 {
-	using cpu = 68000;
 
-	/// This is the Initialiser Table
-	mem_lookup =
-	{
-		{ "ORI", &cpu::ORI, &cpu::CCR, 4 },{ "ORI", &cpu::ORI, &cpu::SR, 5}
-	}
-});
-
-68000::~68000()
-{
-	return 0;
-}
-
-uint8_t 68000::read(uint16_t cpu)
-{
-	return this.bus->read(cpu, false);
-}
-
-void 68000::write(uint16_t a, uint8_t b)
-{
-	this.bus->write(a, b);
 }
