81a7ef9f072ea2137fc2071d558a774d361b5a2e
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-08-02 22:32:11 +0100

    Update 68000.h

--- src/cpu/68000.h
+++ src/cpu/68000.h
@@ -2,30 +2,41 @@
 #include <vector>
 #include <map>
 #include <string>
-#ifndef STDLIB
-#include <stdio.h>
-#endif 
 
-class CPU_HEADER
+#define CPU_BYTE 0
+#define CPU_WORD 1
+#define CPU_LONG 2
+#define CPU_SINGLE 3
+#define CPU_DOUBLE   4
+#define CPU_EXTENDED 5
+#define CPU_PACKED   6
+#define CPU_UNSIZED  7
+
+typedef struct CPU_STATE
 {
-public:
-	typedef struct CPU_REGISTERS
-	{
-		uint32_t ADDRESS_REGISTER;
-		uint32_t PROGRAM_COUNTER;
-		uint8_t INDEX_REGISTER;
-		uint8_t NEGATIVE_REG;
-		uint8_t ZERO_REG;
-		uint8_t OVERFLOW_REG;
-		uint8_t CARRY_OP_REG;
+	uint32_t DATA_REG;
+	uint32_t ADDRESS_REG;
+	uint32_t PROGRAM_COUNTER;
 
-	} REG;
+} STATE;
 
-	typedef struct CPU_FUNC
-	{
-		virtual void RESET();
-		virtual void INTERRUPT();
-		virtual void TIMER();
+typedef struct CPU_REGISTERS
+{
+	uint32_t ADDRESS_REGISTER;
+	uint32_t PROGRAM_COUNTER;
+	uint8_t INDEX_REGISTER;
+	uint8_t NEGATIVE_REG;
+	uint8_t ZERO_REG;
+	uint8_t OVERFLOW_REG;
+	uint8_t CARRY_OP_REG;
 
-	} FUNCTIONS;
-};
+} REG;
+
+typedef struct CPU_FUNCTIONS
+{
+	virtual void RESET();
+	virtual void INTERRUPT();
+	virtual void TIMER();
+
+} FUNC;
+
