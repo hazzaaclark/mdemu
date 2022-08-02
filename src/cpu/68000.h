4f029bc777b3369822c0da9fc3ce2e73aaff266d
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-08-02 13:11:39 +0100

    Add files via upload

---
+++ src/cpu/68000.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <vector>
+#include <map>
+#include <string>
+#ifndef STDLIB
+#include <stdio.h>
+#endif 
+
+typedef struct CPU_REGISTERS
+{
+	uint32_t ADDRESS_REGISTER;
+	uint32_t PROGRAM_COUNTER;
+	uint8_t INDEX_REGISTER;
+	uint8_t NEGATIVE_REG;
+	uint8_t ZERO_REG;
+	uint8_t OVERFLOW_REG;
+	uint8_t CARRY_OP_REG;
+
+} REG;
+
+typedef struct CPU_FUNC
+{
+	virtual void RESET();
+	virtual void INTERRUPT();
+	virtual void TIMER();
+
+} FUNCTIONS;
