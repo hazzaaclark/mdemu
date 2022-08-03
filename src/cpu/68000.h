e57f6c8e2387dc851e3ff5d0884885c811c8fc7c
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-08-03 15:20:06 +0100

    Update 68000.h

--- src/cpu/68000.h
+++ src/cpu/68000.h
@@ -14,67 +14,67 @@
 
 class CPUBus;
 
-class M68K_MAIN
+typedef struct CPU_MAIN
+{
+	/** REGISTERS **/
+
+	uint32_t DATA_REG[8];
+	uint32_t ADDRESS_REG[8];
+	uint32_t PROGRAM_COUNTER;
+	uint32_t STATUS_REGISTER;
+
+	/** POINTERS **/
+
+	int CURRENT_STACK_POINTER;
+	uint32_t STACK_POINTER[3];
+
+	/** FLAGS **/
+
+	uint32_t CONDITION_OP;
+	uint32_t CONDITION_X;
+	uint32_t CONDITION_N;
+	uint32_t CONDITION_V;
+	uint32_t CONDITION_C;
+	uint32_t CONDITION_Z;
+
+} MAIN;
+
+struct CPU_INIT
+{
+	MAIN MASTER_INIT;
+	CPU_MAIN env;
+	typedef struct CPUCLASS cc;
+};
+
+
+typedef struct CPU_REGISTERS
+{
+	uint32_t ADDRESS_REGISTER;
+	uint32_t PROGRAM_COUNTER;
+	uint8_t INDEX_REGISTER;
+	uint8_t NEGATIVE_REG;
+	uint8_t ZERO_REG;
+	uint8_t OVERFLOW_REG;
+	uint8_t CARRY_OP_REG;
+
+} REG;
+
+typedef struct CPU_FUNCTIONS
+{
+	virtual void BUS_INIT(CPUBus);
+	virtual void RESET();
+	virtual void INTERRUPT();
+	virtual void NM_INTERRUPT();
+	virtual void TIMER();
+	bool COMPLETE();
+
+} FUNC;
+
+class
 {
 public:
-	typedef struct CPU_MAIN
-	{
-		/** REGISTERS **/
-
-		uint32_t DATA_REG[8];
-		uint32_t ADDRESS_REG[8];
-		uint32_t PROGRAM_COUNTER;
-		uint32_t STATUS_REGISTER;
-
-		/** POINTERS **/
-
-		int CURRENT_STACK_POINTER;
-		uint32_t STACK_POINTER[3];
-
-		/** FLAGS **/
-
-		uint32_t CONDITION_OP;
-		uint32_t CONDITION_X;
-		uint32_t CONDITION_N;
-		uint32_t CONDITION_V;
-		uint32_t CONDITION_C;
-		uint32_t CONDITION_Z;
-
-	} MAIN;
-
-	struct CPU_INIT
-	{
-		MAIN MASTER_INIT;
-		CPU_MAIN env;
-		typedef struct CPUCLASS cc;
-	};
-
-
-	typedef struct CPU_REGISTERS
-	{
-		uint32_t ADDRESS_REGISTER;
-		uint32_t PROGRAM_COUNTER;
-		uint8_t INDEX_REGISTER;
-		uint8_t NEGATIVE_REG;
-		uint8_t ZERO_REG;
-		uint8_t OVERFLOW_REG;
-		uint8_t CARRY_OP_REG;
-
-	} REG;
-
-	typedef struct CPU_FUNCTIONS
-	{
-		virtual void BUS_INIT(CPUBus);
-		virtual void RESET();
-		virtual void INTERRUPT();
-		virtual void NM_INTERRUPT();
-		virtual void TIMER();
-		bool COMPLETE();
-
-	} FUNC;
-
 	struct MEM_MAP
 	{
-		std::map < uint16_t, std::string> DISASM(uint16_t DIS_START, uint16_t DIS_STOP);
+		std::map <uint16_t, std::string> DISASM(uint16_t DIS_START, uint16_t DIS_STOP);
 	};
 };
