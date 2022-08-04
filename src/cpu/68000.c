ff1851675017a92960f51bfb95e800d32871ef9e
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-08-04 16:50:01 +0100

    Add files via upload

---
+++ src/cpu/68000.c
@@ -0,0 +1,36 @@
+#pragma once
+#include <vector>
+#include <map>
+#include <string>
+#include <stdio.h>
+#include <stdbool.h>
+
+    /** REGISTERS **/
+
+struct REGISTERS
+{
+	uint32_t DATA_REG[8];
+	uint32_t ADDRESS_REG[8];
+	uint32_t PROGRAM_COUNTER;
+	uint32_t STATUS_REGISTER;
+	uint8_t INDEX_REGISTER;
+	uint8_t NEGATIVE_REG;
+	uint8_t ZERO_REG;
+	uint8_t OVERFLOW_REG;
+	uint8_t CARRY_OP_REG;
+};
+
+struct POINTERS
+{
+
+	int CURRENT_STACK_POINTER;
+	uint32_t STACK_POINTER[3];
+
+};
+
+static void BUS_INIT();
+static void CPU_RESET();
+static void REG_INTERRUPT();
+static void NON_MASKABLE_RI();
+static void TIMER();
+bool CYCLECOMPLETE();
