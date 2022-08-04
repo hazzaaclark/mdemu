dcc595f774ab0d042d1bc7a6c37741e891fe2f34
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-08-04 12:02:45 +0100

    Update 68000.cpp

--- src/cpu/68000.cpp
+++ src/cpu/68000.cpp
@@ -2,15 +2,7 @@
 #include <cstdint>
 #include <stdio.h>
 
-static void SET_CPU(CPU_STATE *state)
+M68K::M68K()
 {
-	CPU_STATE* cpu = state;
-	cpu->PROGRAM_COUNTER;
-}
-
-void CPU_FUNCTIONS::RESET()
-{
-	uint32_t ABS_ADDR = 0xFFFFFC;
-	uint32_t READ;
-	void WRITE(uint32_t ADDR_REG, uint32_t DATA_REG);
+	
 }
