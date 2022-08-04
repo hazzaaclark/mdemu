90c5748706d0c38a8f8c48d3980e4ecc227c84a8
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-08-04 16:57:49 +0100

    Update 68000.c

--- src/cpu/68000.c
+++ src/cpu/68000.c
@@ -5,26 +5,19 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-struct REGISTERS
-{
-	uint32_t DATA_REG[8];
-	uint32_t ADDRESS_REG[8];
-	uint32_t PROGRAM_COUNTER;
-	uint32_t STATUS_REGISTER;
-	uint8_t INDEX_REGISTER;
-	uint8_t NEGATIVE_REG;
-	uint8_t ZERO_REG;
-	uint8_t OVERFLOW_REG;
-	uint8_t CARRY_OP_REG;
-};
+uint32_t DATA_REG[8];
+uint32_t ADDRESS_REG[8];
+uint32_t PROGRAM_COUNTER;
+uint32_t STATUS_REGISTER;
+uint8_t INDEX_REGISTER;
+uint8_t NEGATIVE_REG;
+uint8_t ZERO_REG;
+uint8_t OVERFLOW_REG;
+uint8_t CARRY_OP_REG;
 
-struct POINTERS
-{
 
-	int CURRENT_STACK_POINTER;
-	uint32_t STACK_POINTER[3];
-
-};
+int CURRENT_STACK_POINTER;
+uint32_t STACK_POINTER[3];
 
 static void BUS_INIT();
 static void CPU_RESET();
@@ -32,3 +25,8 @@ static void REG_INTERRUPT();
 static void NON_MASKABLE_RI();
 static void TIMER();
 bool CYCLECOMPLETE();
+
+uint16_t CPU_READ(uint16_t a)
+{
+	return 0;
+}
