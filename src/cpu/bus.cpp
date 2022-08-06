023b7aca235711cb437a8c80a9b3d1349596f2ae
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-08-06 01:14:42 +0100

    Update bus.cpp

--- src/cpu/bus.cpp
+++ src/cpu/bus.cpp
@@ -2,5 +2,22 @@
 
 Bus::Bus()
 {
+	for (auto& i : CPU_RAM) i = 0x0000;
+}
 
+Bus::~Bus()
+{
+
+}
+
+void Bus::CPU_WRTIE(uint32_t ADDR, uint32_t DATA)
+{
+	if (ADDR >= 0x000000 && ADDR <= 0xFFFFFF)
+		ADDR = DATA;
+}
+
+void Bus::CPU_READ(uint32_t ADDR, bool READ)
+{
+	if (ADDR >= 0x000000 && ADDR <= 0xFFFFFF)
+		return;
 }
