82435b8366f0143d6951e636025cd33882c5f0ff
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-08-14 22:41:57 +0100

    Update 68000.cpp

--- src/cpu/68000.cpp
+++ src/cpu/68000.cpp
@@ -1,8 +1,6 @@
 #include "68000.h"
 #include "bus.h"
 
-class Bus;
-
 /** CPU CONSTRUCTOR **/
 // Assembles the Operation Code table
 
@@ -25,7 +23,17 @@ void M68K::BUS_INIT()
 	Bus* BUS_POINTER(void** (CPU_READ));
 }
 
+void M68K::CPU_RESET()
+{
+
+}
+
 uint32_t CPU_READ(uint32_t ADDR)
 {
 	return CPU_READ(ADDR);
+}
+
+uint32_t CPU_WRITE(uint32_t ADDR, uint32_t DATA)
+{
+	return CPU_WRITE(ADDR, DATA);
 }
