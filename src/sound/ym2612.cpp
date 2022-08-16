ef0493aa09a64696b94c893f768246fa11a5f44e
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-08-17 00:08:35 +0100

    Update ym2612.cpp

--- src/sound/ym2612.cpp
+++ src/sound/ym2612.cpp
@@ -1,11 +1,13 @@
 #include "ym2612.h"
 
-static void WRITE_REG(uint16_t PORT, uint8_t REG, uint8_t CHANNEL)
+static void WRITE_REG(uint16_t PORT, uint8_t REG, uint8_t CHANNEL, uint8_t VALUE)
 {
 	YM2612::YM_WRITE((PORT * 2) + 0, REG | (CHANNEL * 4));
+	YM2612::YM_WRITE((PORT * 2) + 1, VALUE);
 }
 
-inline void YM2612::YM2612_RESET()
+inline void YM2612::YM_RESET()
 {
-	return YM2612_RESET();
+	return YM_RESET();
 }
+
