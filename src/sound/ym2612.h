4f21f6531771659d0254fed88ad407936103ccc4
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-08-16 23:47:16 +0100

    Update ym2612.h

--- src/sound/ym2612.h
+++ src/sound/ym2612.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <iostream>
+#include <stdio.h>
 
 #ifndef YM2612_H
 #define YM2612_H
@@ -9,8 +11,14 @@ class YM2612
 public:
 	YM2612();
 	~YM2612();
-	static void YM2612_RESET(void);
-};
 
+public:
+	uint8_t YM_READ(const uint16_t PORT);
+
+
+public:
+	static void YM_WRITE(const uint16_t PORT, const uint8_t DATA);
+	static void YM2612_RESET();
+};
 
 #endif 
