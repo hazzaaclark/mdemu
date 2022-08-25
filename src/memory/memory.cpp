952fc76651f1930e4b5aa214442274ea8f87cee1
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-08-25 23:55:38 +0100

    Update memory.cpp

--- src/memory/memory.cpp
+++ src/memory/memory.cpp
@@ -1,11 +1,18 @@
 #include "memory.h"
 #include <string.h>
 
-class MEMORY_MANAGEMENT
+MEM::MEM()
 {
-	static void MEM_INIT()
-	{
-		uint16_t* MEM_PACK();
-		uint16_t* MEM_PACK_EXTERN(uint32_t ADDR);
-	}
-};
+
+}
+
+MEM::~MEM()
+{
+
+}
+
+static void MEM_INIT()
+{
+	uint16_t* MEM_PACK();
+	uint16_t* MEM_PACK_EXTERN(uint32_t ADDR);
+}
