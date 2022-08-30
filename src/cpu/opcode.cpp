58125bf5901d2fee385cf3bceb9f01f3fbe72b97
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-08-30 23:17:32 +0100

    Add files via upload

---
+++ src/cpu/opcode.cpp
@@ -0,0 +1,24 @@
+#include "opcode.h"
+#include <cstdint>
+#include <cstdlib>
+
+const uint32_t OPCODE_DECODER();
+const uint32_t EXTRACT_INSTR(uint32_t B, uint32_t W, uint32_t L)
+{
+	return(B >> (32 - 1 - L)) & ((1 << (L - W + 1)) - 1);
+}
+
+OPCODE::OPCODE()
+{
+	LOOKUP_OPCODE;
+}
+
+inline void OPCODE::LOOKUP_OPCODE(uint32_t CODE)
+{
+	switch (EXTRACT_INSTR(CODE, 0, 3))
+	{
+	case 1: OPCODE_DECODER();
+	case 2:
+		break;
+	}
+}
