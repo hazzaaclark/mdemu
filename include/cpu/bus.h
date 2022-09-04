65331794d00559d406ca16df613f114baeb714a4
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-09-05 00:28:32 +0100

    Update bus.h

--- include/cpu/bus.h
+++ include/cpu/bus.h
@@ -5,19 +5,17 @@
 #include <array>
 #include "68000.h"
 
-class Bus
-{
-public:
-	Bus();
-	~Bus();
+#define BUS_REGISTERS
+uint16_t BUS_RAM[16 * 1000000]{};
+uint16_t CPU_RAM[64 * 1000]; 
 
-public:
-	uint16_t CPU_RAM[128 * 1024];
-	M68K CPU;
+#define BUS_FUNC
+inline void CPU_WRTIE(uint32_t ADDRESS_REG, uint32_t DATA_REG);
+inline void CPU_READ(uint32_t ADDRESS_REG, bool READ_ONLY = false);
+static void BUS_RESET();
+static void BUS_POWER();
 
-public:
-	void CPU_WRTIE(uint32_t ADDRESS_REG, uint32_t DATA_REG);
-	void CPU_READ(uint32_t ADDRESS_REG, bool READ_ONLY = false);
-};
+
+
 
 #endif 
