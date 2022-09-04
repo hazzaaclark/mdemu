dd1f714bf3337f1bd4eedb588de3e7b4f83d85b1
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-09-05 00:28:44 +0100

    Update bus.cpp

--- src/cpu/bus.cpp
+++ src/cpu/bus.cpp
@@ -1,24 +1,19 @@
 #include "bus.h"
 #include "68000.h"
 
-Bus::Bus()
+inline void RAM_INIT()
 {
 	memset((void*)CPU_RAM, 0x0, sizeof(CPU_RAM));
 }
 
 
-Bus::~Bus()
-{
-
-}
-
-void Bus::CPU_WRTIE(uint32_t ADDR, uint32_t DATA)
+inline void BUS_FUNC::CPU_WRTIE(uint32_t ADDR, uint32_t DATA)
 {
 	if (ADDR >= 0x000000 && ADDR <= 0xFFFFFF)
-		ADDR = DATA;
+		ADDR = sizeof(DATA);
 }
 
-void Bus::CPU_READ(uint32_t ADDR, bool READ)
+inline void BUS_FUNC::CPU_READ(uint32_t ADDR, bool READ)
 {
 	if (ADDR >= 0x000000 && ADDR <= 0xFFFFFF)
 		*CPU_RAM = sizeof(ADDR);
