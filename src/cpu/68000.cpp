2d95c79f43e5fce4555ce348085e4c72525026e6
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-09-05 00:58:10 +0100

    Update 68000.cpp

--- src/cpu/68000.cpp
+++ src/cpu/68000.cpp
@@ -5,9 +5,18 @@
 #define READ(ADDR) (BUS = CPU_READ((ADDR)))
 #define WRITE(ADDR, DATA) CPU_WRITE((ADDR), (BUS = (DATA)));
 
-inline void CPU_FLAGS()
+inline void GET_CPU_FLAGS()
 {
-	FLAGS{ C, X, Z, V, N; }
+	FLAGS{ C, X, Z, V, N; };
+}
+
+uint16_t SET_FLAG(uint16_t FLAG)
+{
+	FLAGS::C += FLAG & 1;
+	FLAGS::X += FLAG & 2;
+	FLAGS::Z += FLAG & 4;
+	FLAGS::V += FLAG & 8;
+	FLAGS::N += FLAG & 64;
 }
 
 uint32_t CPU_READ(uint32_t ADDR)
