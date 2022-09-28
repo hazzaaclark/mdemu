e176d7b7be2cab1ad86ed8894c120540bd78439b
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-09-28 12:38:42 +0100

    Update bus.h

--- include/cpu/bus.h
+++ include/cpu/bus.h
@@ -6,6 +6,7 @@
 #include "68000.h"
 
 #define BUS_REGISTERS
+uint16_t BUS_POINTER{};
 uint16_t BUS_RAM[16 * 1000000]{};
 uint16_t CPU_RAM[64 * 1000]; 
 
@@ -14,8 +15,5 @@ inline void CPU_WRTIE(uint32_t ADDRESS_REG, uint32_t DATA_REG);
 inline void CPU_READ(uint32_t ADDRESS_REG, bool READ_ONLY = false);
 static void BUS_RESET();
 static void BUS_POWER();
-
-
-
 
 #endif 
