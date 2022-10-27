1e893dbf5afff8bc93e8ded7fe5f795197b7f594
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-10-27 03:18:46 +0100

    Update 68000.cpp

--- src/cpu/68000.cpp
+++ src/cpu/68000.cpp
@@ -1,57 +1,25 @@
 #include "68000.h"
-#include "bus.h"
-#include "opcode.h"
 
-#define READ(ADDR) (BUS = CPU_READ((ADDR)))
-#define WRITE(ADDR, DATA) CPU_WRITE((ADDR), (BUS = (DATA)));
-#define SET_NZ_FLAG(VALUE) (Z = !VALUE, N = VALUE&0X0)
-#define INTERRUPT_INIT_REQ(INTERRUPT_PENDING) == !REG_INTERRUPT && INTERRUPT_REQUEST || NON_MASKABLE_IR)
-#define INTERRUPT_EXIT_REQ()
+#ifdef DEBUG
+#define DEBUG_LOG() printf(__ARGS__)
+#else
+#define DEBUG_LOG()
+#endif
 
-uint32_t CPU_READ(uint32_t ADDR)
+#define MAIN()
+OPCODE **OPCODE_TABLE;
+
+CPU* CREATE_CPU(MD* CONSOLE)
 {
-	return sizeof(ADDR);
+	calloc(1, sizeof(CPU));
 }
 
-uint32_t CPU_WRITE(uint32_t ADDR, uint32_t DATA)
+static void FREE_CPU_MEM(CPU* MEM)
 {
-	return sizeof(ADDR, DATA);
+	free(MEM);
 }
 
-inline void CPU_INIT()
+inline void CPU_INIT(CPU* CPU)
 {
-	REGISTERS{};
-	REGISTERS::PROGRM_COUNTER = 0;
-	CPU_FUNC::CPU_RESET();
-}
-
-inline static void CPU_RESET()
-{
-	REGISTERS::INTERRUPT_REQUEST = true;
-	REGISTERS::NON_MASKABLE_IR = false;
-	CPU_FUNC::RESETTING = true;
-	CPU_FUNC::RESET_JAMMED = false;
-}
-
-inline void GET_CPU_FLAGS()
-{
-	FLAGS{ C, X, Z, V, N; };
-}
-
-inline void FETCH_EXECUTION()
-{
-	while (!RESETTING)
-	{
-		uint32_t CPU_READ(0xFFFFFFF);
-		return;
-	}
-}
-
-uint16_t SET_CPU_FLAGS(uint16_t FLAG)
-{
-	FLAGS::C += FLAG & 1;
-	FLAGS::X += FLAG & 2;
-	FLAGS::Z += FLAG & 4;
-	FLAGS::V += FLAG & 8;
-	FLAGS::N += FLAG & 64;
+	CPU->ADDRESS_REG[7];
 }
