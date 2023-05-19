bf924fbd39261f7ca22876d94d810333fe3ba691
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2023-05-19 12:55:54 +0100

    Update vdp.h

--- include/video/vdp.h
+++ include/video/vdp.h
@@ -16,6 +16,7 @@
 /* NESTED INCLUDES */
 
 #include "common.h"
+#include "instruction.h"
 
 /* SYSTEM INCLUDES */
 
@@ -50,22 +51,22 @@
 
 #define REGS U8[0x20]
 #define REG5_H40_MODE               U32
-#define REG0_HV_LATCH               BIT(REGS[0], 1) 1
-#define REG0_LINE_IRQ               BIT(REGS[0], 4) 4
-#define REG1_PAL_MODE               BIT(REGS[1], 3) 3
-#define REG1_DMA_ENABLED            BIT(REGS[1], 4) 4
-#define REG1_VBLANK_IRQ             BIT(REGS[1], 5) 5
-#define REG1_DISP_ENABLED           BIT(REGS[1], 6) 6
-#define REG2_NAMETABLE_A            BIT(REGS[2], 3, 3) << 13 13
-#define REG3_NAMETABLE_W            BIT(REGS[3], 1, 5) << 11 11
-#define REG4_NAMETABLE_B            BIT(REGS[4], 0, 3) << 13 13
-#define REG5_SAT_ADDRESS            BIT(REGS[5] & REG5_H40_MODE ? 0x7E : 0x7F)) 0x7E : 0x7F
-#define REG5_SAT_SIZE               BIT(REGS[5], REG5_H40_MODE ? (1 << 10) : (1 << 9)) 1 << 10 : 1 << 9
-#define REG10_LINE_COUNTER          BIT(REGS[10], 0, 8) 8 
-#define REG12_H40_MODE              BIT(REGS[12], 0) 12 
-#define REG15_DMA_INCREMENT         BIT(REGS[15]) 15
-#define REG19_DMA_LENGTH            BIT(REGS[19] || (REGS[20] << 8)) 8
-#define REG23_DMA_TYPE              BIT(REGS[23], 6, 2) 2
+#define REG0_HV_LATCH               0
+#define REG0_LINE_IRQ               0
+#define REG1_PAL_MODE               1
+#define REG1_DMA_ENABLED            1
+#define REG1_VBLANK_IRQ             1
+#define REG1_DISP_ENABLED           1
+#define REG2_NAMETABLE_A            2
+#define REG3_NAMETABLE_W            3
+#define REG4_NAMETABLE_B            4 
+#define REG5_SAT_ADDRESS            5, REG5_H40_MODE
+#define REG5_SAT_SIZE               5, REG5_H40_MODE 
+#define REG10_LINE_COUNTER          10
+#define REG12_H40_MODE              12
+#define REG15_DMA_INCREMENT         15
+#define REG19_DMA_LENGTH            20 << 8
+#define REG23_DMA_TYPE              23
 
 #endif
 
@@ -105,7 +106,7 @@ typedef struct VDP_ARC
 	typedef U32 VDP_PIXEL_CLOCK;
 };
 
-typedef struct GRAPHICS{};
+typedef struct GRAPHICS {};
 
 #endif
 
