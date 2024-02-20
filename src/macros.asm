48c502fb3c90b7e67b2e3249bb3480edc565446e
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-02-20 22:23:02 +0000

    finished macro pritning

--- src/macros.asm
+++ src/macros.asm
@@ -14,7 +14,9 @@
 ;; THIS IS DONE BY DETERMING THE CURRENT STACK POINTER
 ;; IN RELATION TO ALL OF THE DATA AND ADDRESS REGISTERS
 
-DC.B         CHAR_MAX_MASK          255
+DC.B                CHAR_MAX_MASK                               255
+fmt                 DC.B            "%s",                       0
+ERR_MSG             DC.B            "Error",                    0                
 
 PRINT_CHAR:     MACRO
 
@@ -30,10 +32,46 @@ ENDM
 ;; AND EVOKES A RETURN TYPE
 
 PRINT_STRING:   MACRO
+
+    MOVEA.L     #0, A3                        ;; CLEAR A3 CACHE ASSUMING THERE IS ANYTHING THERE
+    MOVEA.L     #1, A3                        ;; SET WRITE MODE
+
+@LOOP\@
+
     MOVE.B      (A3)+,D0            
     CMP.B       #CHAR_MAX_MASK, D0            ;; COMPARE EACH CONSECTUTIVE BYTE IN D0, 255 IS THE MAX MASK
     JSR         PRINT_CHAR                    ;; JUMP TO THIS INSTRUCTION TO PRINT OUT EVERY CONSECTUTIVE ELEMENT
-    BRA         PRINT_STRING                  ;; BRANCH ALWAYS TO THIS SPECIFIC INSTRUCTION ON EVERY EXECUTION OF PRINT_CHAR
+    BRA         @LOOP\@                       ;; BRANCH ALWAYS TO THIS SPECIFIC INSTRUCTION ON EVERY EXECUTION OF PRINT_CHAR
+
+    RTS
+
+ENDM
+
+;; AS A OONJUNCTIVE EFFORT OF ALL OF THE PREVIOUS MACROS
+;; THIS WILL BE THE MAIN ENCOMPASSING MACRO FOR PRINTING MESSAGES
+;; AND VARIOUS OTHER INPUTS
+
+;; SEE https://www.nxp.com/docs/en/reference-manual/M68000PRM.pdf#page=74
+;; FOR INFORMATION PERTAINING TOWARDS DATA FORMATTING
+
+PRINT_SEMANTIC(fmt, ...)
+
+    LINK        A6, #-8                         ;; ALLOCATE FOR LOCAL VAR
+    MOVEA.L     #fmt, A0                        ;; LOAD MESSAGE FORMATTING INTO A0
+    CMP.L       #0, D0
+    BNE.S       @LOOP\@                         ;; LOOP THROUGH THE STRING WHILE THERE ARE CHARS AVAILABLE IN THE STACK       
+    BRA         FREE                            ;; ONCE FINISHED, FREE/EXIT THE PROGRAM
+
+@LOOP\@
+
+     MOVE.L     4(A6), A1                       ;; LOAD THE FIRST VARIADIC ARG FROM THE MACRO
+     MOVEA.L    A0, -(A7)                       ;; PUSH MESSAGE ONTO THE STACK
+     JSR        PRINT_SEMANTIC
+
+FREE:               MACRO 
+
+    UNLK    A6
+    RTS
 
 ENDM
 
